7a7af7dedd7c8688bb9682133a3807c0140bae41
Amine Kchelfi <aminekchelfi@gmail.com>
2021-11-30 23:21:50 +0100

    Prova

--- ProvaDelSecolo.java
+++ ProvaDelSecolo.java
@@ -5,6 +5,7 @@ public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("Ciao Mondo! prova del secolo");
 		System.out.println("Ciao Mondo! prova!");
+		System.out.println("Ciao Gay! prova!");
 	}
 	
 
